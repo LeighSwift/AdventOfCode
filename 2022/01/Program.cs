b3d48ff0ec777ae4e2e79b3e26601e490307290c
Leigh Swift <leighswift@Leighs-MacBook-Pro-M1.local>
2022-12-01 15:14:06 +0000

    2002-01 C# solution

---
+++ 2022/01/Program.cs
@@ -0,0 +1,23 @@
+﻿const string InputFile = @"/Users/leighswift/p4/depot/usr/Leigh.Swift/AdventOfCode/2022/01/input.txt";
+
+string[] inputLines = File.ReadAllLines(InputFile);
+
+int elfCalories = 0;
+int[] topThree = new int[3] {0,0,0};
+for (int lineIdx = 0; lineIdx < inputLines.Length; lineIdx++)
+{
+    string line = inputLines[lineIdx];
+    if (string.IsNullOrWhiteSpace(line) || (lineIdx+1) == inputLines.Length)
+    {
+        topThree[0] = Math.Max(topThree[0], elfCalories);
+        Array.Sort(topThree);
+        elfCalories = 0;
+        continue;
+    }
+    elfCalories += int.Parse(line);
+}
+
+// Answer to part one.
+Console.WriteLine($"Highest:          {topThree[2]}");
+// Answer to part two.
+Console.WriteLine($"Sum of top three: {topThree.Sum()}");
---
+++ 2022/01/cpp/main.cpp
@@ -0,0 +1,6 @@
+const char *inputFilename = "/Users/leighswift/p4/depot/usr/Leigh.Swift/AdventOfCode/2022/01/input.txt";
+
+int main()
+{
+    
+}
