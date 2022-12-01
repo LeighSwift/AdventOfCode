6f94f0170a8ed562ebe0cd11d10d335fc9267e38
Leigh Swift <me@leighswift.com>
2022-12-01 18:36:09 +0000

    2015 Day 02

--- 2015/main.cpp
+++ 2015/main.cpp
@@ -2,6 +2,11 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <array>
+#include <algorithm>
+
+// https://adventofcode.com/2015
 
 namespace AoC
 {
@@ -17,6 +22,20 @@ namespace AoC
                 std::stringstream buffer;
                 buffer << inputFile.rdbuf();
                 result = buffer.str();
+            }
+            return result;
+        }
+        static std::vector<std::string> ReadAllLines(const char *filename)
+        {
+            std::vector<std::string> result;
+            std::ifstream inputFile(filename);
+            if (inputFile.is_open())
+            {
+                std::string line;
+                while (std::getline(inputFile, line))
+                {
+                    result.push_back(std::move(line));
+                }
             }
             return result;
         }
@@ -43,21 +62,47 @@ void day01()
         default:
             break;
         }
-        if(floor == -1 && firstBasementEntryPos == 0)
+        if (floor == -1 && firstBasementEntryPos == 0)
         {
-            firstBasementEntryPos = i+1;
+            firstBasementEntryPos = i + 1;
         }
     }
     std::cout << "AoC: Day 01: Final floor:     " << floor << std::endl;
     std::cout << "AoC: Day 01: Enters basement: " << firstBasementEntryPos << std::endl;
+    std::cout << std::endl;
 }
 
 void day02()
 {
-    
+    std::vector<std::string> inputData = AoC::FileSystem::ReadAllLines("input02.txt");
+    int totalWrappingSqFt = 0;
+    int totalRibbonFeet = 0;
+    for (size_t i = 0; i < inputData.size(); i++)
+    {
+        std::string &line = inputData[i];
+        std::istringstream lineStream(line);
+        int l, w, h;
+        char x;
+        lineStream >> l >> x >> w >> x >> h;
+        totalWrappingSqFt += (2 * l * w) + (2 * w * h) + (2 * h * l);
+        totalWrappingSqFt += std::min(std::min(l * w, w * h), h * l);
+
+        std::array<int, 3> sides = {l, w, h};
+        std::sort(sides.begin(), sides.end());
+
+        totalRibbonFeet += l * w * h;
+        totalRibbonFeet += sides[0];
+        totalRibbonFeet += sides[0];
+        totalRibbonFeet += sides[1];
+        totalRibbonFeet += sides[1];
+    }
+    std::cout << "AoC: Day 02: Total wrapping sqft: " << totalWrappingSqFt << std::endl;
+    std::cout << "AoC: Day 02: Total ribbon feet:   " << totalRibbonFeet << std::endl;
+    std::cout << std::endl;
 }
 
 int main()
 {
     day01();
+    day02();
 }
