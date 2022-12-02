9f6ee5c2de92dc32dfe3db26b116890120a6fbca
Leigh Swift <me@leighswift.com>
2022-12-02 10:14:33 +0000

    Fix formatting

--- 2022/main.cpp
+++ 2022/main.cpp
@@ -47,8 +47,8 @@ namespace AoC
 void day01()
 {
     int elfCalories = 0;
-    std::vector<int> topThree {0, 0, 0};
-    
+    std::vector<int> topThree{0, 0, 0};
+
     std::vector<std::string> inputData = AoC::FileSystem::ReadAllLines("input01.txt");
     for (size_t i = 0; i < inputData.size(); i++)
     {
@@ -68,71 +68,71 @@ void day01()
     std::cout << std::endl;
 }
 
-    void day02()
+void day02()
+{
+    std::map<char, int> shapeScores;
+    shapeScores['A'] = 1;
+    shapeScores['B'] = 2;
+    shapeScores['C'] = 3;
+    std::map<char, char> winningMoves;
+    winningMoves['A'] = 'B';
+    winningMoves['B'] = 'C';
+    winningMoves['C'] = 'A';
+    std::map<char, char> losingMoves;
+    losingMoves['A'] = 'C';
+    losingMoves['B'] = 'A';
+    losingMoves['C'] = 'B';
+
+    int totalScoreP1 = 0;
+    int totalScoreP2 = 0;
+    std::vector<std::string> inputData = AoC::FileSystem::ReadAllLines("input02.txt");
+    for (size_t i = 0; i < inputData.size(); i++)
     {
-        std::map<char, int> shapeScores;
-        shapeScores['A'] = 1;
-        shapeScores['B'] = 2;
-        shapeScores['C'] = 3;
-        std::map<char, char> winningMoves;
-        winningMoves['A'] = 'B';
-        winningMoves['B'] = 'C';
-        winningMoves['C'] = 'A';
-        std::map<char, char> losingMoves;
-        losingMoves['A'] = 'C';
-        losingMoves['B'] = 'A';
-        losingMoves['C'] = 'B';
-
-        int totalScoreP1 = 0;
-        int totalScoreP2 = 0;
-        std::vector<std::string> inputData = AoC::FileSystem::ReadAllLines("input02.txt");
-        for (size_t i = 0; i < inputData.size(); i++)
+        std::string &line = inputData[i];
+        char opponentMove = line[0];
+        // Part 1
         {
-            std::string &line = inputData[i];
-            char opponentMove = line[0];
-            // Part 1
-            {
-                char yourMove = line[2] - 23;
+            char yourMove = line[2] - 23;
 
-                // First do total score for story part 1 incorrect assumption that 'X' == 'A' etc
-                totalScoreP1 += shapeScores[yourMove];
-                if (opponentMove == yourMove)
-                {
-                    totalScoreP1 += 3;
-                }
-                else if (yourMove == winningMoves[opponentMove])
-                {
-                    totalScoreP1 += 6;
-                }
-            }
-            // Part 2 - Now figure out correct answer part 2.
+            // First do total score for story part 1 incorrect assumption that 'X' == 'A' etc
+            totalScoreP1 += shapeScores[yourMove];
+            if (opponentMove == yourMove)
             {
-                char yourMove;
-                switch (line[2])
-                {
-                case 'X':
-                    yourMove = losingMoves[opponentMove];
-                    break;
-                case 'Y':
-                    yourMove = opponentMove;
-                    totalScoreP2 += 3;
-                    break;
-                case 'Z':
-                    yourMove = winningMoves[opponentMove];
-                    totalScoreP2 += 6;
-                    break;
-                }
-                totalScoreP2 += shapeScores[yourMove];
+                totalScoreP1 += 3;
+            }
+            else if (yourMove == winningMoves[opponentMove])
+            {
+                totalScoreP1 += 6;
             }
         }
-
-        std::cout << "AoC: Day 02: Total score part 1: " << totalScoreP1 << std::endl;
-        std::cout << "AoC: Day 02: Total score part 2: " << totalScoreP2 << std::endl;
-        std::cout << std::endl;
+        // Part 2 - Now figure out correct answer part 2.
+        {
+            char yourMove;
+            switch (line[2])
+            {
+            case 'X':
+                yourMove = losingMoves[opponentMove];
+                break;
+            case 'Y':
+                yourMove = opponentMove;
+                totalScoreP2 += 3;
+                break;
+            case 'Z':
+                yourMove = winningMoves[opponentMove];
+                totalScoreP2 += 6;
+                break;
+            }
+            totalScoreP2 += shapeScores[yourMove];
+        }
     }
 
-    int main()
-    {
-        day01();
-        day02();
-    }
+    std::cout << "AoC: Day 02: Total score part 1: " << totalScoreP1 << std::endl;
+    std::cout << "AoC: Day 02: Total score part 2: " << totalScoreP2 << std::endl;
+    std::cout << std::endl;
+}
+
+int main()
+{
+    day01();
+    day02();
+}
