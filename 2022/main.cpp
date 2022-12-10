2bde677873afb3661181208b13c142335d33afd4
Leigh Swift <me@leighswift.com>
2022-12-10 11:03:01 +0000

    2022 Day 10

--- 2022/main.cpp
+++ 2022/main.cpp
@@ -858,11 +858,64 @@ void day09()
 
 void day10()
 {
-    // std::vector<std::string> inputData = AoC::FileSystem::ReadAllLines("input10.txt");
+    std::vector<std::string> display;
+    int xReg = 1;
+    int cycle = 0;
+    int totalSignal = 0;
+
+    auto clock = [&](int i, int nX)
+    {
+        const int pixel = i % 40;
+        if (pixel == 0)
+        {
+            display.push_back("");
+        }
+        display.back() += abs(xReg - pixel) <= 1 ? '#' : ' ';
+        if (pixel == 19)
+        {
+            totalSignal += (i + 1) * xReg;
+        }
+        xReg = nX;
+    };
+
+    std::vector<std::string> inputData = AoC::FileSystem::ReadAllLines("input10.txt");
+    for (size_t i = 0; i < inputData.size(); i++)
+    {
+        std::istringstream lineStream(inputData[i]);
+        std::string op;
+        lineStream >> op;
+        if (op == "noop")
+        {
+            clock(cycle++, xReg);
+        }
+        else if (op == "addx")
+        {
+            int val;
+            lineStream >> val;
+            clock(cycle++, xReg);
+            clock(cycle++, xReg + val);
+        }
+    }
+
+    std::cout << "AoC: Day 10: Part 1:  " << totalSignal << std::endl;
+    std::cout << "AoC: Day 10: Part 2:" << std::endl;
+    for (const std::string &line : display)
+    {
+        std::cout << line << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void day11()
+{
+    // std::vector<std::string> inputData = AoC::FileSystem::ReadAllLines("input11.txt");
     // for (size_t i = 0; i < inputData.size(); i++)
     // {
     //     std::string &line = inputData[i];
     // }
+    // std::cout << "AoC: Day 10: Part 1:  " << todo << std::endl;
+    // std::cout << "AoC: Day 10: Part 2:  " << todo << std::endl;
+    // std::cout << std::endl;
 }
 
 int main()
@@ -877,4 +930,5 @@ int main()
     day08();
     day09();
     day10();
+    day11();
 }
