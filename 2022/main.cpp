151fe30298ac9f09f3c383dec4aba538c66f94df
Leigh Swift <me@leighswift.com>
2022-12-15 13:40:21 +0000

    2022 Day 13

--- 2022/main.cpp
+++ 2022/main.cpp
@@ -1157,14 +1157,14 @@ void day13()
     class Day13
     {
     public:
-        static void FindNextElem(char *&str)
+        static void FindNextElem(const char *&str)
         {
             if (*str == '[' || *str == ',')
             {
                 ++str;
             }
         }
-        static void SkipInteger(char *&str)
+        static void SkipInteger(const char *&str)
         {
             while (*str >= '0' && *str <= '9')
             {
@@ -1183,77 +1183,64 @@ void day13()
             }
             else
             {
-                // lhs > rhs
                 return 2;
             }
         }
-        static int Eval(char *&lhs, int rhs)
+        static int Eval(const char *&lhs, int rhs)
         {
             char newArr[10];
             snprintf(newArr, 10, "[%d]", rhs);
-            char *newArrPtr = newArr;
+            const char *newArrPtr = newArr;
             return Eval(lhs, newArrPtr);
         }
-        static int Eval(int lhs, char *&rhs)
+        static int Eval(int lhs, const char *&rhs)
         {
             char newArr[10];
             snprintf(newArr, 10, "[%d]", lhs);
-            char *newArrPtr = newArr;
+            const char *newArrPtr = newArr;
             return Eval(newArrPtr, rhs);
         }
-        static int Eval(char *&lhs, char *&rhs, bool *bBothArrayEnd = nullptr)
+        static int Eval(const char *&lhs, const char *&rhs)
         {
-            if (*lhs == '[')
+            if (*lhs == ']' && *rhs != ']')
             {
-                if (*rhs == '[')
-                {
-                    int rtn = 1;
-                    bool bBothArrayEnd = false;
-                    while (rtn == 1 && !bBothArrayEnd)
-                    {
-                        FindNextElem(lhs);
-                        FindNextElem(rhs);
-                        rtn = Eval(lhs, rhs, &bBothArrayEnd);
-                    }
-                    return rtn;
-                }
-                else
-                {
-                    // mix
-                    int rhsInt = atoi(rhs);
-                    SkipInteger(rhs);
-                    return Eval(lhs, rhsInt);
-                }
+                return 0;
             }
-            else if (*rhs == '[')
+            else if (*lhs != ']' && *rhs == ']')
             {
-                // mix
+                return 2;
+            }
+            else if (*lhs == '[' && *rhs == '[')
+            {
+                int rtn = 1;
+                while (rtn == 1)
+                {
+                    FindNextElem(lhs);
+                    FindNextElem(rhs);
+                    if (*lhs == ']' && *rhs == ']')
+                    {
+                        ++lhs;
+                        ++rhs;
+                        return 1;
+                    }
+                    rtn = Eval(lhs, rhs);
+                }
+                return rtn;
+            }
+            else if (*lhs == '[' && *rhs != '[')
+            {
+                int rhsInt = atoi(rhs);
+                SkipInteger(rhs);
+                return Eval(lhs, rhsInt);
+            }
+            else if (*lhs != '[' && *rhs == '[')
+            {
                 int lhsInt = atoi(lhs);
                 SkipInteger(lhs);
                 return Eval(lhsInt, rhs);
             }
             else
             {
-                // if both arrays end, we continue so return 1
-                if (*lhs == ']' && *rhs == ']')
-                {
-                    ++lhs;
-                    ++rhs;
-                    if (bBothArrayEnd)
-                        *bBothArrayEnd = true;
-                    return 1;
-                }
-                // if lhs array ends before rhs, correct order
-                else if (*lhs == ']' && *rhs != ']')
-                {
-                    return 0;
-                }
-                // if rhs array ends before lhs, wrong order
-                else if (*lhs != ']' && *rhs == ']')
-                {
-                    return 2;
-                }
-                // otherwise both integers
                 int lhsInt = atoi(lhs);
                 int rhsInt = atoi(rhs);
                 SkipInteger(lhs);
@@ -1261,20 +1248,44 @@ void day13()
                 return Eval(lhsInt, rhsInt);
             }
         }
+        static int Eval(const std::string &inLhs, const std::string &inRhs)
+        {
+            const char *lhs = inLhs.c_str();
+            const char *rhs = inRhs.c_str();
+            return Day13::Eval(lhs, rhs);
+        }
+    };
+
+    class Packet
+    {
+    public:
+        bool operator<(const Packet &rhs) const
+        {
+            return Day13::Eval(message, rhs.message) == 0;
+        }
+
+        bool operator==(const Packet &other) const
+        {
+            return message == other.message;
+        }
+
+        std::string message;
     };
 
     std::vector<std::string> inputData = AoC::FileSystem::ReadAllLines("input13.txt");
     std::vector<int> correctOrder;
     std::vector<int> incorrectOrder;
+    std::vector<Packet> packets;
     int index = 1;
     for (size_t i = 1; i < inputData.size(); i += 3)
     {
         std::string &left = inputData[i - 1];
         std::string &right = inputData[i];
 
-        char *leftPtr = &left[0];
-        char *rightPtr = &right[0];
-        int result = Day13::Eval(leftPtr, rightPtr);
+        packets.push_back({left});
+        packets.push_back({right});
+
+        int result = Day13::Eval(left, right);
         if (result == 0)
         {
             correctOrder.push_back(index);
@@ -1287,8 +1298,18 @@ void day13()
         assert(result != 1);
     }
 
-    std::cout << "AoC: Day 13: Part 1:  " << 0 << std::endl;
-    std::cout << "AoC: Day 13: Part 2:  " << 0 << std::endl;
+    int correctIndexSum = std::accumulate(correctOrder.begin(), correctOrder.end(), 0);
+
+    Packet divOne = {"[[2]]"};
+    Packet divTwo = {"[[6]]"};
+    packets.push_back(divOne);
+    packets.push_back(divTwo);
+    std::sort(packets.begin(), packets.end());
+    int divOneIndex = std::find(packets.begin(), packets.end(), divOne) - packets.begin() + 1;
+    int divTwoIndex = std::find(packets.begin(), packets.end(), divTwo) - packets.begin() + 1;
+
+    std::cout << "AoC: Day 13: Part 1:  " << correctIndexSum << std::endl;
+    std::cout << "AoC: Day 13: Part 2:  " << divOneIndex * divTwoIndex << std::endl;
     std::cout << std::endl;
 }
 
